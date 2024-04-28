5fcdfcf55fc02e65af93e16314e3df3e19b49b62
Alejandro Dahlgren <alejandro.dahlgren@gmail.com>
2024-04-28 11:56:55 +0200

    restartar for att det var för mycket fel

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,25 @@
-import "./App.css";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-import Navbar from "./Navbar";
+import {createBrowserRouter,createRoutesFromElements,Route, RouterProvider} from "react-router-dom";
 import Countries, {countryDetailsLoader} from "../pages/Countries";
+import Rootlayout from "../layouts/Rootlayout";
+import Home from "../pages/Home";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <>
-      <Route path="/" element={<Navbar />} />
-      <Route path="/" element={<Countries />} loader={countryDetailsLoader} />
-    </>
+    <Route path="/" element={<Rootlayout />}>
+      <Route index element={<Home />}></Route>
+      <Route path="country/:id" element={<countryDetailsLoader />} loader={countryLoader} />
+    </Route>
   )
 );
 
 function App() {
-  return <RouterProvider router={router} />;
-}
+  return (
+    <>
+      <RouterProvider router={router} />;
+    </>
+)}
 
 export default App;
+
+
+
---
+++ src/layouts/Rootlayout.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+
+const Rootlayout = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    )
+}
+
+export default Rootlayout;
